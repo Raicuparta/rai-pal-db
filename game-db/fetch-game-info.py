8a7fbb2f271bf92f33c580d124a499fa99a3881e
Raicuparta <Raicuparta@users.noreply.github.com>
2024-08-04 15:22:03 +0200

    add epic ids

--- game-db/fetch-game-info.py
+++ game-db/fetch-game-info.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+import re
 
 # If any backwards-incompatible changes are made to the database, increment this number.
 # This will be used as the folder name where the database files are stored,
@@ -10,6 +11,21 @@ database_version = 0
 # PCGamingWiki seems to allow for this many elements per page for now.
 # If this limit goes down, we might have problems.
 limit_per_page = 500
+
+# Someone was nice enough to do all the work for mapping Epic Games Store item IDs to game titles.
+epic_games_store_items = "https://raw.githubusercontent.com/nachoaldamav/items-tracker/main/database/titles.json"
+
+
+def normalize_title(title):
+    return re.sub(r'\W+', '', title).lower()
+
+
+def fetch_epic_games_store_items():
+    # array where shape is { "id": "item_id", "title": "game_title" }
+    response = requests.get(epic_games_store_items)
+
+    # map where keys are normalized titles, and values are the ids
+    return {normalize_title(item['title']): item['id'] for item in response.json()}
 
 
 def fetch_games_by_engine(engine_name):
@@ -47,6 +63,14 @@ def fetch_games_by_engine(engine_name):
     return all_games
 
 
+# array where shape is { "id": "item_id", "title": "game_title" }
+epic_games_store_items = fetch_epic_games_store_items()
+
+
+def get_epic_game_id_from_title(title):
+    return epic_games_store_items.get(normalize_title(title), None)
+
+
 def clean_up_properties(game):
     cleaned_game = {k: v for k, v in game.items() if v is not None}
 
@@ -59,18 +83,21 @@ def clean_up_properties(game):
     if 'gogIds' in cleaned_game:
         cleaned_game['gogIds'] = cleaned_game['gogIds'].split(',')
 
+    epic_id = get_epic_game_id_from_title(cleaned_game['title'])
+    if epic_id:
+        cleaned_game['epicIds'] = [epic_id]
+
     return cleaned_game
 
 
-engine_names = ["Unity", "Godot", "Unreal", "GameMaker"]
+output_path = f"{database_version}/games.json"
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+engine_names = ["GameMaker", "Unity", "Godot", "Unreal"]
 
 all_games = []
 for engine_name in engine_names:
     games = fetch_games_by_engine(engine_name)
     all_games.extend(clean_up_properties(game) for game in games)
-
-output_path = f"{database_version}/games.json"
-os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
-with open(output_path, "w") as json_file:
-    json.dump(all_games, json_file, indent=4)
+    with open(output_path, "w") as json_file:
+        json.dump(all_games, json_file, indent=4)
