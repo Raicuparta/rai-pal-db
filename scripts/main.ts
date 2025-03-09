d0f3be81ea8f6e5e6df1c3615e433f12a05d08a9
Raicuparta <Raicuparta@users.noreply.github.com>
2025-03-09 04:49:34 +0100

    woops double fetch

--- scripts/main.ts
+++ scripts/main.ts
@@ -61,7 +61,6 @@ async function main(pretty: boolean) {
       fetchSteamGames(),
       fetchPCGamePassGames(),
       fetchEAGamePassGames(),
-      fetchPCGamePassGames(),
       fetchEpicGamesStoreGames(),
       fetchUbisoftGames(),
     ])
