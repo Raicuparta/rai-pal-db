b65acccf541a12b75c3f353c1b7d133eeeba6b2d
Raicuparta <Raicuparta@users.noreply.github.com>
2026-05-25 20:11:28 +0200

    stable mod db json

--- scripts/mod-db/main.ts
+++ scripts/mod-db/main.ts
@@ -1,13 +1,20 @@
 import { MOD_DATABASE_VERSION } from "#common/versions.ts";
 import { getMods } from "./mods/mods.ts";
+import stringify from "canonical-json";
+
+const json = stringify(
+  {
+    mods: await getMods(),
+  },
+  undefined,
+  2,
+);
+
+if (!json) {
+  throw new Error("Failed to convert mods output to JSON");
+}
 
 await Deno.writeTextFile(
-	`../mod-db/${MOD_DATABASE_VERSION}/mods.json`,
-	JSON.stringify(
-		{
-			mods: await getMods(),
-		},
-		null,
-		2,
-	),
+  `../mod-db/${MOD_DATABASE_VERSION}/mods.json`,
+  json,
 );
