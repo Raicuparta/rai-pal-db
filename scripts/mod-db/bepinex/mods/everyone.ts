644d2fd2dfa0ec78d1dc7c04fbe3b941185f8c4b
Raicuparta <Raicuparta@users.noreply.github.com>
2026-06-15 17:10:04 +0200

    update everyone

--- scripts/mod-db/bepinex/mods/everyone.ts
+++ scripts/mod-db/bepinex/mods/everyone.ts
@@ -6,14 +6,15 @@ export async function getEveryoneMods() {
 		bepinexMod({
 			id: "everyone-mono",
 			unityBackend: "Mono",
-			title: "Everyone",
+			title: "Everyone (Mono)",
 			author: "Raicuparta",
 			sourceCode: "https://github.com/Raicuparta/everyone",
-			description: "Mono version. Work in progress! F3 to chat.",
+			description:
+				"Adds multiplayerish features. Requires being logged in to Rai Pal. F3 to chat.",
 			download: {
-				id: "0.2.0",
+				id: "0.2.1",
 				url:
-					"https://github.com/Raicuparta/rai-pal-db/releases/download/everyone-v0.2.0/EveryoneClient.BepInEx5.Mono.zip",
+					"https://github.com/Raicuparta/rai-pal-db/releases/download/everyone-v0.2.1/EveryoneClient.BepInEx5.Mono.zip",
 			},
 		}, {
 			configFileName: "raicuparta.everyone.json",
@@ -21,14 +22,15 @@ export async function getEveryoneMods() {
 		bepinexMod({
 			id: "everyone-il2cpp",
 			unityBackend: "Il2Cpp",
-			title: "Everyone",
+			title: "Everyone (Il2Cpp)",
 			author: "Raicuparta",
 			sourceCode: "https://github.com/Raicuparta/everyone",
-			description: "IL2CPP version. Work in progress! F3 to chat.",
+			description:
+				"Adds multiplayerish features. Requires being logged in to Rai Pal. F3 to chat.",
 			download: {
-				id: "0.2.0",
+				id: "0.2.1",
 				url:
-					"https://github.com/Raicuparta/rai-pal-db/releases/download/everyone-v0.2.0/EveryoneClient.BepInEx.Unity.IL2CPP.CoreCLR.zip",
+					"https://github.com/Raicuparta/rai-pal-db/releases/download/everyone-v0.2.1/EveryoneClient.BepInEx.Unity.IL2CPP.CoreCLR.zip",
 			},
 		}, {
 			configFileName: "raicuparta.everyone.json",
