75017dcd6115b7d5183052addf5e7db69b43a8be
rai <Raicuparta@users.noreply.github.com>
2026-09-13 14:23:20 +0200

    cleanup and update

--- scripts/mod-db/bepinex/mods/uuvr.ts
+++ scripts/mod-db/bepinex/mods/uuvr.ts
@@ -4,77 +4,81 @@ import { bepinexMod } from "../bepinex-mod.ts";
 // deno-lint-ignore require-await
 export async function getUuvrMods(): Promise<ModBase[]> {
 	return [
-		bepinexMod({
-			id: "uuvr-il2cpp-legacy",
-			family: "uuvr",
-			unityBackend: "Il2Cpp",
-			title: "UUVR Il2Cpp Legacy",
-			author: "Raicuparta",
-			sourceCode: "https://github.com/Raicuparta/uuvr",
-			description:
-				"Uses OpenVR. Start SteamVR before starting the game. Use the Configuration Manager mod to change UUVR settings for each game.",
-			engineVersionRange: {
-				maximum: {
-					major: 2019,
+		bepinexMod(
+			{
+				id: "uuvr-il2cpp-legacy",
+				family: "uuvr",
+				unityBackend: "Il2Cpp",
+				title: "UUVR Il2Cpp Legacy",
+				author: "Raicuparta",
+				sourceCode: "https://github.com/Raicuparta/uuvr",
+				description:
+					"Uses OpenVR. Start SteamVR before starting the game. Use the Configuration Manager mod to change UUVR settings for each game.",
+				engineVersionRange: {
+					maximum: {
+						major: 2019,
+					},
+				},
+				download: {
+					id: "0.3.1",
+					url: "https://github.com/Raicuparta/uuvr/releases/download/v0.3.1/uuvr-il2cpp-legacy.zip",
 				},
 			},
-			download: {
-				id: "0.3.1",
-				url:
-					"https://github.com/Raicuparta/uuvr/releases/download/v0.3.1/uuvr-il2cpp-legacy.zip",
+			{
+				withPatchers: true,
+				configFileName: "raicuparta.uuvr-legacy.cfg",
 			},
-		}, {
-			withPatchers: true,
-			configFileName: "raicuparta.uuvr-legacy.cfg",
-		}),
-		bepinexMod({
-			id: "uuvr-mono-legacy",
-			family: "uuvr",
-			unityBackend: "Mono",
-			title: "UUVR Mono Legacy",
-			author: "Raicuparta",
-			sourceCode: "https://github.com/Raicuparta/uuvr",
-			description:
-				"Uses OpenVR. Start SteamVR before starting the game. Use the Configuration Manager mod to change UUVR settings for each game.",
-			engineVersionRange: {
-				maximum: {
-					major: 2019,
+		),
+		bepinexMod(
+			{
+				id: "uuvr-mono-legacy",
+				family: "uuvr",
+				unityBackend: "Mono",
+				title: "UUVR Mono Legacy",
+				author: "Raicuparta",
+				sourceCode: "https://github.com/Raicuparta/uuvr",
+				description:
+					"Uses OpenVR. Start SteamVR before starting the game. Use the Configuration Manager mod to change UUVR settings for each game.",
+				engineVersionRange: {
+					maximum: {
+						major: 2019,
+					},
+				},
+				download: {
+					id: "0.3.1",
+					url: "https://github.com/Raicuparta/uuvr/releases/download/v0.3.1/uuvr-mono-legacy.zip",
 				},
 			},
-			download: {
-				id: "0.3.1",
-				url:
-					"https://github.com/Raicuparta/uuvr/releases/download/v0.3.1/uuvr-mono-legacy.zip",
+			{
+				withPatchers: true,
+				configFileName: "raicuparta.uuvr-legacy.cfg",
 			},
-		}, {
-			withPatchers: true,
-			configFileName: "raicuparta.uuvr-legacy.cfg",
-			withPatchers: true,
-		}),
-		bepinexMod({
-			id: "uuvr-mono-modern",
-			family: "uuvr",
-			unityBackend: "Mono",
-			title: "UUVR Mono Modern",
-			author: "Raicuparta",
-			sourceCode: "https://github.com/Raicuparta/uuvr",
-			description:
-				"Supports OpenXR and OpenVR. Use the Configuration Manager mod to change UUVR settings for each game.",
-			engineVersionRange: {
-				minimum: {
-					major: 2018,
-					minor: 4,
+		),
+		bepinexMod(
+			{
+				id: "uuvr-mono-modern",
+				family: "uuvr",
+				unityBackend: "Mono",
+				title: "UUVR Mono Modern",
+				author: "Raicuparta",
+				sourceCode: "https://github.com/Raicuparta/uuvr",
+				description:
+					"Supports OpenXR and OpenVR. Use the Configuration Manager mod to change UUVR settings for each game.",
+				engineVersionRange: {
+					minimum: {
+						major: 2018,
+						minor: 4,
+					},
+				},
+				download: {
+					id: "0.4.0",
+					url: "https://github.com/Raicuparta/uuvr/releases/download/v0.4.0/uuvr-mono-modern.zip",
 				},
 			},
-			download: {
-				id: "0.4.0",
-				url:
-					"https://github.com/Raicuparta/uuvr/releases/download/v0.4.0/uuvr-mono-modern.zip",
+			{
+				withPatchers: true,
+				configFileName: "raicuparta.uuvr-modern.cfg",
 			},
-		}, {
-			withPatchers: true,
-			configFileName: "raicuparta.uuvr-modern.cfg",
-			withPatchers: true,
-		}),
+		),
 	];
 }
